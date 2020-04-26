0b8b2e2e91aa65061f7773e45bb36f32c22ac09e
welisonmenezes <welisonmenezes@hotmail.com>
2020-04-26 19:41:57 -0300

    Remove python byte files

--- application/app.py
+++ application/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
+import sys
+sys.dont_write_bytecode = True
 
 # create a Flask app
 app = Flask(__name__, template_folder='templates', static_folder='static')
