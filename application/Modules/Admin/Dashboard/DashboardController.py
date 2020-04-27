c3f3864ae8a3c7a2f010205522f905724b222a1d
welisonmenezes <welisonmenezes@hotmail.com>
2020-04-27 14:30:46 -0300

    Add database tests and examples by using sqlalchemy

--- application/Modules/Admin/Dashboard/DashboardController.py
+++ application/Modules/Admin/Dashboard/DashboardController.py
@@ -1,5 +1,6 @@
 from flask import current_app, Blueprint, render_template, request, url_for, redirect, flash, session, jsonify
 from app import app
+from Modules.Database import Session
 
 DashboardController = Blueprint('DashboardController', __name__, url_prefix='/admin', template_folder='Views', static_folder='static')
 
